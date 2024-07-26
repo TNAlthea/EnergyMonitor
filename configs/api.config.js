e37188daa85949ff8bae86b6b81f5b25e433dd79
TNAlthea <sabiannaya@yahoo.com>
2024-07-26 12:55:59 +0700

    feat: added config

--- configs/api.config.js
+++ configs/api.config.js
@@ -5,4 +5,5 @@ const BASE_URL = process.env.API_BASE_URL;
 module.exports = {
   API_STORE_ELECTRICITY_DATA: `${BASE_URL}/api/electricity/add`,
   API_STORE_ANOMALY_DATA: `${BASE_URL}/api/electricity/anomaly/add`,
+
 };
